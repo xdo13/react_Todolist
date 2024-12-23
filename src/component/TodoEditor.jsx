b1fe9afc4b66207986389ae4e8e5a19d18d5e4a9
xdo13 <xdo13@naver.com>
2024-12-23 15:18:38 +0900

    TodoList Crud까지

---
+++ src/component/TodoEditor.jsx
@@ -0,0 +1,36 @@
+import { useState, useRef } from "react";
+import "./TodoEditor.css";
+
+const TodoEditor = ({ onCreate }) => {
+    const [content, setContent] = useState("");
+    const inputRef = useRef();
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    };
+    const onSubmit = () => {
+        if (!content) {
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent(""); // 저장후 문자열 초기화
+    };
+    const onKeyDown = (e) => {
+        if(e.keyCode === 13) {
+            onSubmit();
+        }
+    };
+
+    return (
+     <div className="TodoEditor">
+        <h4>새로운 Todo 작성하기🖊</h4>
+        <div className="editor_wrapper">
+            <input ref={inputRef} value={content} onChange={onChangeContent} onKeyDown={onKeyDown} placeholder="새로운 Todo..." />
+            <button onClick={onSubmit}>추가</button>
+        </div>
+     </div>
+    );
+};
+
+export default TodoEditor;
+
